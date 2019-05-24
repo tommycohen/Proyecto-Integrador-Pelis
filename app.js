5acf9b6aca57f05180d558c9b86e21ddb2714cb9
Jonathan <jbrener@udesa.edu,ar>
2019-05-24 10:33:40 -0300

    Cree el genero, edite footer. Hay que agregarle el logo de tmdb

--- app.js
+++ app.js
@@ -3,7 +3,22 @@ window.onload = function(){
 
 
 
+  window.onclick = function(e) {
+    var myDropdown = document.getElementById("myDropdown");
 
+    if (!e.target.matches('.dropbtn')) {
+
+
+
+        myDropdown.classList.remove('show');
+
+    } else {
+
+
+        myDropdown.classList.add('show');
+      
+    }
+  }
 
 
 
