d1b003947c58268f8feb1aab677c6ad2b17931a0
Jonathan <jbrener@udesa.edu,ar>
2019-05-26 20:05:29 -0300

    Division de las partes de footer

---
+++ app.js
@@ -0,0 +1,25 @@
+window.onload = function(){
+
+
+
+
+  window.onclick = function(e) {
+    var myDropdown = document.getElementById("myDropdown");
+
+    if (!e.target.matches('.dropbtn')) {
+
+
+
+        myDropdown.classList.remove('show');
+
+    } else {
+
+
+        myDropdown.classList.add('show');
+      
+    }
+  }
+
+
+
+}
