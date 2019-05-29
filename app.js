f751ecea44db88036b85c7f301094fc6f73d41a8
Jonathan <jbrener@udesa.edu,ar>
2019-05-29 11:45:24 -0300

    Principio de linkeo con la API

--- app.js
+++ app.js
@@ -26,4 +26,18 @@ window.onclick = function(event) {
     }
 }
 
+var URL = "https://api.themoviedb.org/3/movie/latest?api_key=186cf2a9c1ccf8eb8f856d9b1c0cfe5f&language=en-US"
+
+fetch(URL)
+.then(function(response) {
+return response.json()
+})
+.then(function(information) {
+console.log(information);
+console.log(information.results);
+console.log(information.results[0]);
+})
+.catch(function(error) {
+console.log("Error: " + error);
+})
 }
