41b27ac6a40e2a935fc83780c1a4f057967fe1fa
tommycohen <tcohen@udesa.edu.ar>
2019-06-04 14:07:29 -0300

    Carrousel

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 window.onload = function(){
-  var urlEstrenos = "https://api.themoviedb.org/3/movie/upcoming?api_key=ebcc7d54892cbf6b9dd7c194bd052f2f&page=1"
-
+  var urlEstrenos = "https://api.themoviedb.org/3/movie/popular?api_key=ebcc7d54892cbf6b9dd7c194bd052f2f&language=en-US&page=1"
+ var urlConstante = "https://image.tmdb.org/t/p/original/"
 fetch(urlEstrenos)
   .then(function(response) {
     return response.json();
@@ -10,9 +10,9 @@ fetch(urlEstrenos)
 
     var arrayDeEstrenos= data.results
 
-    for (var i = 0; i < 3; i++) {
+    for (var i = 0; i < 1; i++) {
       var imagen = data.results[i].poster_path
-      document.querySelector('div.carousel-item.active').innerHTML = "<img src='" + imagen + "'class='d-block w-100' alt='350px'>"
+      document.querySelector('div.carousel-item.active').innerHTML += "<img src='" + urlConstante + imagen + "'class='d-block w-100' alt='350px'>"
     }
     var a = document.querySelector('.carousel-item .active')
     console.log(a);
@@ -24,6 +24,28 @@ fetch(urlEstrenos)
 
 
 
+  var urlEstrenos = "https://api.themoviedb.org/3/movie/popular?api_key=ebcc7d54892cbf6b9dd7c194bd052f2f&language=en-US&page=1"
+  var urlConstante = "https://image.tmdb.org/t/p/original/"
+  fetch(urlEstrenos)
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(data) {
+    console.log(data.results);
+
+    var arrayDeEstrenos= data.results
+
+    for (var i = 0; i < 1; i++) {
+      var imagen = data.results[i].poster_path
+      document.querySelector('#carouselDos').innerHTML += "<img src='" + urlConstante + imagen + "'class='d-block w-100' alt='350px'>"
+    }
+    var a = document.querySelector('.carousel-item .active')
+    console.log(a);
+  })
+
+  .catch(function(error) {
+  console.log('Hubo un problema con la petición Fetch:' + error.message);
+  });
 
 
 
