ab73ebf65f5a991085329b1e0694c7f2c6e1c226
tommycohen <tcohen@udesa.edu.ar>
2019-06-06 18:14:03 -0300

    arreglso

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-window.onload = function(){
+window.addEventListener("load", function(){
 //app y appold.js//
   window.onclick = function(e) {
     var myDropdown = document.getElementById("myDropdown");
@@ -109,97 +109,8 @@ fetch(urlEstrenos)
   });
 
 
-//buscador.js//
-  localStorage.removeItem("moneda")
-  console.log(localStorage);
-
-  var queryString = new URLSearchParams(location.search)
-  var buscador = queryString.get("buscador")
-
-  fetch("https://api.themoviedb.org/3/search/movie?api_key=7beff09c9fd5a79010c5ce4883840da9&query=" + buscador +"&page=1&include_adult=true")
-    .then(function(respuesta) {
-      return respuesta.json()
-    })
-    .then(function(informacion) {
-      console.log(informacion);
-
-      var arrayDeGifs = informacion.data
-      console.log(arrayDeGifs);
-
-      for (var i = 0; i < arrayDeGifs.length; i++) {
-        var id = arrayDeGifs[i].id
-        var title = arrayDeGifs[i].title
-        var url = arrayDeGifs[i].images.original.url
-
-        document.querySelector("div").innerHTML += "<p><a href=detalleGif.html?id=" + id + ">" + title + "</a></p>"
-        document.querySelector("div").innerHTML += "<img src=" + url + ">"
-      }
-    })
-    .catch(function(error) {
-      console.log("Error: " + error);
-    })
-
-//estrenos.js//
-
-
-  var urlEstrenos = "https://api.themoviedb.org/3/movie/upcoming?api_key=ebcc7d54892cbf6b9dd7c194bd052f2f&page=1"
-
-fetch(urlEstrenos)
-  .then(function(response) {
-    return response.json();
-  })
-  .then(function(data) {
-    console.log(data.results);
-
-    var arrayDeEstrenos= data.results
-
-    for (var i = 0; i < arrayDeEstrenos.length; i++) {
-      var id = data.results[i].id
-      var titulo = data.results[i].title
-      var imagen = data.results[i].poster_path
-      console.log(titulo)
-      document.querySelector(".contenedor").innerHTML += "<article class='peliculas'><img class='estrenos-imagenes' src='https://image.tmdb.org/t/p/original" + imagen + "' alt='img producto'><h2 class='titulo-producto'><a href=detallePelicula.html?id=" + id +  ">" + titulo + "</a></h2></article>"
-    }
-  })
-
-  .catch(function(error) {
-  console.log('Hubo un problema con la petición Fetch:' + error.message);
-  });
-
-//
-
-//estrenos.js//
-
-  var urltop100 = "https://api.themoviedb.org/3/movie/top_rated?api_key=ebcc7d54892cbf6b9dd7c194bd052f2f&page=1"
-
-fetch(urltop100)
-  .then(function(response) {
-    return response.json();
-  })
-  .then(function(data) {
-    console.log(data.results);
-    var  arrayDePopulares = data.results
-
-
-    for (var i = 0; i < arrayDePopulares.length; i++) {
-      var id = data.results[i].id
-      var titulo = data.results[i].title
-      var imagen = data.results[i].poster_path
-      console.log(titulo)
-      document.querySelector(".contenedor").innerHTML += "<article class='peliculas'><img class='imagenes' src='https://image.tmdb.org/t/p/original" + imagen + "' alt='img producto'><h2 class='titulo-producto'><a href=detallePelicula.html?id=" + id +  ">" + titulo + "</a></h2></article>"
-    }
-  })
-
-  .catch(function(error) {
-  console.log('Hubo un problema con la petición Fetch:' + error.message);
-  });
-
-
-
-//
 
 
 
 
-
-}
+})
