ad2c3ad91519856baf26ebc8cb1d76434f3e2371
tommycohen <tcohen@udesa.edu.ar>
2019-06-13 17:37:20 -0300

    minimo avance en detalle pelicula

--- js/detallePelicula.js
+++ js/detallePelicula.js
@@ -1,7 +1,7 @@
 window.addEventListener("load", function(){
 
-  var queryString = new URLSearchParams(location.search)
-  var detalles = queryString.get("detalles")
+  var queryString = new URLSearchParams(location.search);
+  var detalles = queryString.get('id');
 
   var detallePelicula = "https://api.themoviedb.org/3/movie/" + detalles + "?api_key=ebcc7d54892cbf6b9dd7c194bd052f2f&language=en-US"
 
@@ -12,16 +12,17 @@ fetch(detallePelicula)
 .then(function(data) {
   console.log(data.results);
 
-  var arrayDeEstrenos= data.results
+  var resultadoPelicula = informacion.results
 
-  for (var i = 0; i < 1; i++) {
-    var id = data.results[i].id
-    var titulo = data.results[i].title
-    var imagen = data.results[i].poster_path
-    console.log(titulo)
-    document.querySelector(".contenedor").innerHTML += "<article class='peliculas'><img class='estrenos-imagenes' src='https://image.tmdb.org/t/p/original" + imagen + "' alt='img producto'><h2 class='titulo-producto'><a href=detallePelicula.html?id=" + id +  ">" + titulo + "</a></h2></article>"
-  }
-})
+    for (var i = 0; i < resultadoPelicula.length; i++) {
+      var id = informacion.results[i].id
+      var titulo = informacion.results[i].title
+      var imagen = informacion.results[i].poster_path
+      
+      }
+      console.log(titulo)
+      document.querySelector(".contenedor_busqueda").innerHTML += "<article class='peliculas'><img class='estrenos-imagenes' src='" + imagen + "' alt='img producto'><h2 class='titulo-producto'><a href=detallePelicula.html?id=" + id +  ">" + titulo + "</a></h2></article>"
+    }
 
 .catch(function(error) {
 console.log('Hubo un problema con la petición Fetch:' + error.message);
